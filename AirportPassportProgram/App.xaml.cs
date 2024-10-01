f482d55a2e8e0953b631c676a956a57dbb38e5b1
rogersm-uwosh <91431120+rogersm-uwosh@users.noreply.github.com>
2024-10-01 13:26:43 -0500

    Lab4-First Commit

---
+++ AirportPassportProgram/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace AirportPassportProgram;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AirportsPage();
+	}
+}
---
+++ AirportPassportProgram/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using AirportPassportProgram.Model;
+
+namespace AirportPassportProgram;
+
+public static class MauiProgram
+{
+	    public static IBusinessLogic BusinessLogic = new BusinessLogic(new Database());
+
+
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
